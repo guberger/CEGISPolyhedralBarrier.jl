d892d6e82bb97c7358523a6f40417386f8e22cae
guberger <guillaume.berger@colorado.edu>
2023-06-30 09:49:59 -0600

    Clean Exa ICE Consensus

--- examples/exa_ICE_Consensus_2D/main.jl
+++ examples/exa_ICE_Consensus_2D/main.jl
@@ -3,9 +3,9 @@ module ExampleICEConsensus2D
 include("../utils/toolkit.jl")
 
 N = 3 # x, y, t
-vmax = 3
-vmin = -3
-T = 8
+vmax = 10
+vmin = -10
+T = vmax - vmin
 
 guards_xy = [
     [AffForm([-1, 1, 0], 1)],
@@ -13,8 +13,14 @@ guards_xy = [
     [AffForm([-1, 1, 0], 0), AffForm([1, -1, 0], 0)]
 ]
 guards_t = [
-    [[AffForm([0, 0, 1], -(T - 2))], [AffForm([0, 0, -1], T - 1)]],
-    [AffForm[], AffForm[]]
+    [
+        [AffForm([0, 0, -1], 0), AffForm([0, 0, 1], -(T - 2))],
+        [AffForm([0, 0, -1], T - 1), AffForm([0, 0, 1], -(T - 1))],
+    ],
+    [
+        AffForm[],
+        [AffForm([0, 0, -1], T), AffForm([0, 0, 1], -T)]
+    ]
 ]
 As = [[1 0 0; 0 1 0; 0 0 1], [1 0 0; 0 1 0; 0 0 0]]
 bs = [
@@ -34,14 +40,14 @@ end
 fig = figure(0, figsize=(10, 5))
 ax = fig.add_subplot()
 
-nstep = 10
-ax.set_xlim((0, nstep))
+nstep = 15
+ax.set_xlim((0, nstep - 1))
 ax.set_xlabel("time")
 
 dv = vmax - vmin
 ax.set_ylim((vmin - 0.05*dv, vmax + 0.05*dv))
 for y in (vmin, vmax)
-    ax.plot((0, nstep), (y, y), c="k")
+    ax.plot((0, nstep - 1), (y, y), c="k")
 end
 ax.set_ylabel("x, y")
 
@@ -53,39 +59,37 @@ for istep = 1:nstep
     @assert state.loc ∈ (1, 2)
     @assert state.loc != 2 || state.x[3] ≈ T
     marker = state.loc == 1 ? "x" : "."
-    ax.plot((istep - 1), state.x[1], marker=marker, ms=5, c="blue")
-    ax.plot((istep - 1), state.x[2], marker=marker, ms=5, c="red")
+    ax.plot((istep - 1), state.x[1], marker=marker, ms=10, c="blue")
+    ax.plot((istep - 1), state.x[2], marker=marker, ms=10, c="red")
     istep == nstep && break
     state = next_state(pieces, state, tol_dom)
 end
 
+η = 0.1
+
 prob = BarrierProblem(
     N, pieces,
+    GenForm[], # gfs_inv
     [
-        GenForm(1, AffForm([0, 0, -1], 0)),
-        GenForm(1, AffForm([0, 0, 1], -(T - 1))),
-        GenForm(2, AffForm([0, 0, 1], -T)),
-        GenForm(2, AffForm([0, 0, -1], T))
-    ], # gfs_inv
-    [
-        [GenForm(loc, AffForm([-1, 0, 0], vmin - 1)) for loc = 1:2]...,
-        [GenForm(loc, AffForm([1, 0, 0], -vmax - 1)) for loc = 1:2]...,
-        [GenForm(loc, AffForm([0, -1, 0], vmin - 1)) for loc = 1:2]...,
-        [GenForm(loc, AffForm([0, 1, 0], -vmax - 1)) for loc = 1:2]...,
-        GenForm(2, AffForm([1, -1, 0], -0.5)),
-        GenForm(2, AffForm([-1, 1, 0], -0.5))
+        [GenForm(loc, AffForm([-1, 0, 0], vmin - η)) for loc = 1:2]...,
+        [GenForm(loc, AffForm([1, 0, 0], -vmax - η)) for loc = 1:2]...,
+        [GenForm(loc, AffForm([0, -1, 0], vmin - η)) for loc = 1:2]...,
+        [GenForm(loc, AffForm([0, 1, 0], -vmax - η)) for loc = 1:2]...,
+        GenForm(2, AffForm([1, -1, 0], - η)),
+        GenForm(2, AffForm([-1, 1, 0], - η))
     ], # gfs_safe
     [
         State(1, [vmin, vmin, 0]), State(1, [vmin, vmax, 0]),
         State(1, [vmax, vmin, 0]), State(1, [vmax, vmax, 0])
     ], # states_init
-    0.05, # ϵ
+    η/10, # ϵ
     0.0 # δ
 )
 
 iter_max = Inf
 status, gen_prob, rec = CPB.find_barrier(prob, iter_max,
-                                         solver, print_period=10)
+                                         solver, print_period=10,
+                                         int_gen=true, int_verif=true)
 @assert status == CPB.BARRIER_FOUND
 
 # Illustration
@@ -107,32 +111,28 @@ for ax in ax_
     ax.tick_params(axis="both", labelsize=15)
 end
 
-gfs = vcat(gen_prob.gfs, prob.gfs_inv)
+gfs = [GenForm(gf.loc, AffForm(gf.af.a, ceil(gf.af.β))) for gf in gen_prob.gfs]
+display(gfs)
 
 for loc = 1:2
     plot_level3D!(ax_[loc], gfs, loc, lims,
                   fc="red", fa=0.1, ec="red", ew=0.5)
 end
 
-# for loc = 1:4
-#     plot_level2D!(ax_[loc], gen_prob.gfs, loc, lims,
-#                   fc="gold", ec="gold", fa=0.5, ew=2.5)
-# end
+for state in gen_prob.states_inside
+    plot_point!(ax_[state.loc], state.x, mc="blue")
+end
 
-# for state in gen_prob.states_inside
-#     plot_point!(ax_[state.loc], state.x, mc="blue")
-# end
+for state in gen_prob.states_image
+    plot_point!(ax_[state.loc], state.x, mc="purple")
+end
 
-# for state in gen_prob.states_image
-#     plot_point!(ax_[state.loc], state.x, mc="purple")
-# end
+for link in gen_prob.links_unknown
+    state = link.src
+    plot_point!(ax_[state.loc], state.x, mc="orange")
+end
 
-# for link in gen_prob.links_unknown
-#     state = link.src
-#     plot_point!(ax_[state.loc], state.x, mc="orange")
-# end
-
-# @assert isempty(gen_prob.states_outside)
+@assert isempty(gen_prob.states_outside)
 
 ## Algo illustration
 fig = figure(2, figsize=(10, 5))
